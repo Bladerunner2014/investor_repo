fbae46c62b9482aaf81b4c7fbcbed01a01cfe8b1
Mohammad Hossein Heidary <mohammadhsein9@gmail.com>
2023-02-03 16:26:13 +0330

    clear

--- blueprint.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-
-v1_blueprint = Blueprint('v1', __name__, url_prefix="/v1")
-v2_blueprint = Blueprint('v2', __name__)
-
--- constants/enums.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class SubLevel(Enum):
-    blue = 1
-    yellow = 2
-    bronze = 3
-    silver = 4
-    gold = 5
