23a639ccefc9c0cbeb837922b6fb3c78f35104f9
Mohammad Hossein Heidary <mohammadhsein9@gmail.com>
2023-02-03 16:33:18 +0330

    docker final

---
+++ models/investors.py
@@ -0,0 +1,21 @@
+import datetime
+from datetime import timezone
+
+
+# Class of investors
+class InvestorsDB:
+    __table_name__ = 'test_inv'
+    id = 'id'
+    user_id = 'user_id'
+    api_key = 'api_key'
+    # secret_key = 'secret_key'
+    is_subscribe = 'is_subscribe'
+    exchange = 'exchange'
+    expire_date = 'expire_date'
+    sub_level = 'sub_level'
+    created_at = 'created_at'
+    updated_at = 'updated_at'
+
+    def __init__(self):
+        self.created_at = datetime.datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
+        self.updated_at = datetime.datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S.%f")
